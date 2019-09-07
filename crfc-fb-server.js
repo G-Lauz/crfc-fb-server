e1964c3983cea7631048cdb315b0f8547e250458
G-Lauz <gabriellauzier77@gmail.com>
2019-09-07 15:13:05 -0400

    add yargs command

--- crfc-fb-server.js
+++ crfc-fb-server.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 // /crfc-fb-server.js //
 
 'use strict';
@@ -5,16 +6,107 @@
 require('dotenv').config();
 
 const express = require('express'),
-      bodyParser = require('body-parser');
+      bodyParser = require('body-parser'),
+      yargs = require('yargs');
 
 // required middlewares
 var server = express().use(bodyParser.json()); // creates express http server
 
-// Routes
-server.use('/app/chatbot', require('messenger-chatbot'));
-server.use('/app/emplois', require('tab-emplois'));
-server.get('/', function(req, res) {
-  console.log('test crfc-fb-server.js');
-});
+function start_server() {
+  // Routes
+  server.use('/app/chatbot', require('messenger-chatbot'));
+  server.use('/app/emplois', require('tab-emplois'));
+  server.get('/', function(req, res) {
+    console.log('test crfc-fb-server.js');
+  });
 
-server.listen(process.env.PORT || 1337, () => console.log('Server is listening'));
+  // Server listening
+  server.listen(process.env.PORT || 1337, () => console.log('Server is listening'));
+}
+
+const argv = yargs
+  .scriptName("crvfc")
+  .command('start [app]', 'Start the designated application', {
+    app: {
+      description: "path leading to the application, if none, the server will start",
+      type: 'string'
+    }
+  }, (argv) => {
+    if (argv.app){
+      console.log("starting up the", argv.app);
+    } else {
+      console.log("starting up the server");
+      start_server();
+    }
+  })
+  .command('stop [app]', 'Stop the designated application', {
+    app: {
+      description: "path leading to the application, if none, the server will stop",
+      type: 'string'
+    }
+  }, (argv) => {
+    if (argv.app){
+      console.log("shutting down the", argv.app);
+    } else {
+      console.log("shutting down the server");
+      // TODO
+    }
+  })
+  .usage("Usage:")
+  .options('verbose', {
+    alias: 'v',
+    description: 'Verbose the output',
+    type: 'boolean'
+  })
+  .help()
+  .alias('help', 'h')
+  .locale('fr')
+  .argv;
+
+console.log(argv);
+
+/*// input manager
+var stdin = process.openStdin();
+
+// input listener
+stdin.addListener("data", function(obj){
+  var arg = obj.toString().trim().split(' ');
+
+  if(arg[0] != "crfc"){
+    console.log("Commande invalide");
+    return;
+  }
+
+  console.log(arg);
+  switch (arg[1]) {
+  case "app":
+    switch (arg[2]) {
+      case "-a":
+      case "--add":
+        console.log("add that app");
+        break;
+      case "-rm":
+      case "--remove":
+        console.log("remove that app");
+        break;
+      case "-v":
+      case "--verbose":
+        console.log("verbose that app");
+        break;
+      default:
+        console.log("Commande invalide");
+        console.log(arg[2]);
+    }
+    break;
+  case "tree":
+    console.log("show tree");
+    break;
+  case "help":
+    console.log("show help");
+    break;
+  default:
+    console.log("Commande invalide");
+    console.log(arg[1]);
+  }
+});
+*/
